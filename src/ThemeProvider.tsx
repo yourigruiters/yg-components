880f35696a336677c807ce7f5e4bbde89cf69a12
yourigruiters <i-youri-i@hotmail.com>
2021-03-27 21:45:53 +0100

    Toggle between light and dark mode implemented - removed themeprovider

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import { globalTheme, GlobalThemes } from "./theme";
 import { ThemeProvider } from "@emotion/react";
 
-export default (props) => {
-  const [theme, setTheme] = useState<GlobalThemes>("light");
+const Theme = (props) => {
+	const [theme, setTheme] = useState<GlobalThemes>("light");
 
-  console.log(setTheme);
+	const handleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+	};
 
-  return (
-    <ThemeProvider theme={globalTheme[theme]}>{props.children}</ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={globalTheme[theme]}>
+			<button onClick={handleTheme}>Toggle</button>
+			{props.children}
+		</ThemeProvider>
+	);
 };
+
+export default Theme;
